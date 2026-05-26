650908aff5d775f6d2755f25e8ddd2f5cc0e48ef
Haji490 <99gkwldnjs99@gmail.com>
2026-05-26 12:30:40 +0900

    mockData, const, type

---
+++ src/constants/EmissionFactors.ts
@@ -0,0 +1,9 @@
+/**
+ * 배출계수
+ */
+export const EMISSION_FACTORS = {
+  gasoline: 2.1,     
+  electricity: 0.45, 
+  transport: 0.12,
+  material: 2.5    
+};
---
+++ src/types/dto.ts
@@ -0,0 +1,31 @@
+export type GhgEmission = {
+    yearMonth: string; 
+    source: 'gasoline' | 'lpg' | 'electricity' | 'purchased_materials' | 'logistics';
+    scope: 'Scope 1' | 'Scope 2' | 'Scope 3';
+    emissions: number; 
+}
+
+export type Company = {
+    id: string;
+    name: string;
+    country: string; 
+    emissions: GhgEmission[];
+}
+
+export type ProductPCF = {
+    id: string;
+    name: string;
+    gasoline: number    //L
+    electricity: number; // 전기 사용량 (kWh)
+    material: number; // 원소재 입력값 (kg)
+    transport: number; // 운송 거리 (km)
+    totalPcf: number; //결과값 (kgCO2e)
+};
+
+export type Post = {
+    id: string;
+    title: string;
+    resourceUid: string;
+    dateTime: string;    
+    content: string;
+};
